268807efa3a6060b85971b0b43596c3c21ffe334
karlssof <karlssof@MTL-6PTTTT3>
2024-01-15 15:21:29 +0100

    Add project files.

---
+++ smtestapp/sm.vb
@@ -0,0 +1,28 @@
+﻿Imports System
+Imports SMScriptBox
+Imports Microsoft.VisualBasic
+Imports System.IO
+Imports System.Net
+Imports System.Text
+
+
+<Serializable()> Public Class Script
+	Sub New()
+	End Sub
+    Shared Sub Main()
+        Try
+            FlowContext.SetResult(1)
+            Dim phoneNumber As String = FlowContext.GetSystemVariable("@@MediaLib.ANI")
+            FlowContext.Print("#### Phonenumber: " + phoneNumber)
+            phoneNumber = "+46" + phoneNumber.Substring(1)
+            FlowContext.SetVariable("@IntPhoneNumber", phoneNumber)
+            FlowContext.SetResult(2)
+
+        Catch ex As Exception
+            FlowContext.Print("### Error:" & ex.Message)
+            FlowContext.SetResult(9)
+        End Try
+    End Sub
+
+
+End Class
---
+++ smtestapp/smcriptbox.vb
@@ -0,0 +1,52 @@
+﻿
+Public Class FlowContext
+    Shared varibles As New Dictionary(Of String, String)
+    Public Shared Sub Print(txt As String)
+        Console.WriteLine(txt)
+    End Sub
+    Public Shared Sub SetResult(i As Integer)
+        Console.WriteLine("result: " + i.ToString())
+    End Sub
+
+    Public Shared Function GetSystemVariable(varible As String) As String
+        Try
+            Return varibles(varible)
+        Catch
+            Console.WriteLine("The varible do not exist: " + varible)
+            Return ""
+        End Try
+    End Function
+    Public Shared Function GetVariable(varible As String) As String
+        Try
+            Return varibles(varible)
+        Catch
+            Console.WriteLine("The varible do not exist: " + varible)
+            Return ""
+        End Try
+    End Function
+    Public Shared Sub SetVariable(varible As String, value As String)
+        Console.WriteLine("Varible set: " + varible + " to: " + value)
+        If varibles.ContainsKey(varible) Then
+            varibles(varible) = value
+        Else
+            varibles.Add(varible, value)
+        End If
+    End Sub
+    Shared Sub AddDebugVaibles()
+        With varibles
+            .Add("@@MediaLib.ANI", "0708111222")
+            .Add("@@MediaLib.CallID", "1234")
+        End With
+    End Sub
+End Class
+
+
+<Serializable()> Public Class Startup
+
+    Shared Sub Main()
+        FlowContext.AddDebugVaibles()
+        Script.Main()
+        Console.ReadLine()
+    End Sub
+
+End Class
